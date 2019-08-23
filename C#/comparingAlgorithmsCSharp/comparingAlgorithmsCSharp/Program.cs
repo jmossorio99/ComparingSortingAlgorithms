17341d7d7cfbc588f8b617ed8bd50d7c32334866
juandavid2025 <juan.aguirre1@correo.icesi.edu.co>
2019-08-23 17:33:01 -0500

    added c

---
+++ C#/comparingAlgorithmsCSharp/comparingAlgorithmsCSharp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace comparingAlgorithmsCSharp
+{
+    class Program
+    {
+
+        public Program()
+        {
+
+            var sw = Stopwatch.StartNew();
+
+
+            var elapsed = sw.ElapsedMilliseconds;
+
+            Console.WriteLine(""+elapsed);
+        }
+
+        static void Main(string[] args)
+        {
+
+            Program program = new Program();
+
+        }
+    }
+}
