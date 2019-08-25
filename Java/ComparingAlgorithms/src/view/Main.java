2a5a6e0fda3dd0d75ebe3fb19a21ec2d89bb69bc
Marco Perez <marcopza99@hotmail.com>
2019-08-25 16:21:47 -0500

    Full implementation of both Treesort and Heapsort in java

--- Java/ComparingAlgorithms/src/view/Main.java
+++ Java/ComparingAlgorithms/src/view/Main.java
@@ -4,9 +4,10 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Arrays;
+import java.util.ArrayList;
 
 import model.Heap;
+import model.AVLTree;
 
 public class Main {
 
@@ -14,58 +15,78 @@ public class Main {
 		
 		File file10000 = new File("Data/set10000.txt");
 		File file100000 = new File("Data/set100000.txt");
-		File file1000000 = new File("Data/set100000.txt");
-		executeAlgorithms(file10000, 10000);
-		executeAlgorithms(file100000, 100000);
-		executeAlgorithms(file1000000, 1000000);
-//		int[] test1 = {9,8,7,6,5,4,3,2,1,0};
-//		int[] test2 = {9,8,7,6,5,4,3,2,1,0};
-//		Heap heap = new Heap(test1, test1.length + 2);
-//		int[] result = heap.heapSort(10);
-//		for (int i = 0; i < result.length; i++) {
-//			System.out.print(" " + result[i]);
-//		}
-//		System.out.print("]");
-//		Arrays.sort(test2);
-//		System.out.print("[");
-//		for (int i = 0; i < test2.length; i++) {
-//			System.out.print(" " + test2[i]);
-//		}
-//		System.out.print("]");
+		File file1000000 = new File("Data/set1000000.txt");
+		long timeTaken11 = executeAlgorithms(file10000, 10000, 1);
+		System.out.println("Heapsort takes: " + timeTaken11);
+		long timeTaken12 = executeAlgorithms(file10000, 10000, 0);
+		System.out.println("Treesort takes: " + timeTaken12);
+		long timeTaken21 = executeAlgorithms(file100000, 100000, 1);
+		System.out.println("Heapsort takes: " + timeTaken21);
+		long timeTaken22 = executeAlgorithms(file100000, 100000, 0);
+		System.out.println("Treesort takes: " + timeTaken22);
+		long timeTaken31 = executeAlgorithms(file1000000, 1000000, 1);
+		System.out.println("Heapsort takes: " + timeTaken31);
+		long timeTaken32 = executeAlgorithms(file1000000, 1000000, 0);
+		System.out.println("Treesort takes: " + timeTaken32);
 	}
 
-	public static void executeAlgorithms(File file, int size) {
+	public static void print(int[] arr, int n) {
 		
-		try {
-			BufferedReader buff = new BufferedReader(new FileReader(file));
-			int[] first = new int[size];
-			int[] second = new int[size];
-			String current = buff.readLine();
-			int currentNum = 0;
-			while(current != null) {
-				first[currentNum] = Integer.parseInt(current);
-				second[currentNum] = Integer.parseInt(current);
-				currentNum++;
-				current = buff.readLine();
+        for (int i = 0; i < n; i++) { 
+            System.out.print(arr[i] + " "); 
+        } 
+        System.out.print("\n"); 
+        
+    } 
+	
+	public static long executeAlgorithms(File file, int size, int algorithm) {
+		if(algorithm == 0) {
+			try {
+				BufferedReader buff = new BufferedReader(new FileReader(file));
+				int[] previous = new int[size];
+				String current = buff.readLine();
+				AVLTree avl = new AVLTree();
+				while(current != null) {
+					avl.insert(Integer.parseInt(current));
+					current = buff.readLine();
+				}
+				long startTime = System.currentTimeMillis();
+				ArrayList result = new ArrayList<>();
+				avl.inOrder(avl.getRoot(), result);
+				long endTime = System.currentTimeMillis();
+				long total = endTime - startTime;
+				buff.close();
+				return total;
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
-			long startTime = System.currentTimeMillis();
-			Arrays.sort(first);
-			long endTime = System.currentTimeMillis();
-			long total = endTime - startTime;
-			System.out.println("Timsort takes: "+ total + "ms");
-			Heap heap = new Heap(second, second.length + 2);
-			startTime = System.currentTimeMillis();
-			heap.heapSort(size);
-			endTime = System.currentTimeMillis();
-			total = endTime - startTime;
-			System.out.println("Heapsort takes: "+ total + "ms");
-			buff.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
+		}else {
+			try {
+				BufferedReader buff = new BufferedReader(new FileReader(file));
+				int[] previous = new int[size];
+				String current = buff.readLine();
+				int currentNum = 0;
+				while(current != null) {
+					previous[currentNum] = Integer.parseInt(current);
+					currentNum++;
+					current = buff.readLine();
+				}
+				Heap heap = new Heap(previous, previous.length + 2);
+				long startTime = System.currentTimeMillis();
+				heap.heapSort(size);
+				long endTime = System.currentTimeMillis();
+				long total = endTime - startTime;
+				buff.close();
+				return total;
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
-		
+		return -1;
 	}
 	
 }
