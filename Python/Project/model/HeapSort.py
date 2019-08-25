8f6efcc0617cfe11ba341727a8466dbedcc505fc
Jose <joseossorio99@hotmail.com>
2019-08-25 12:56:54 -0500

    worked on heapsort on the Python project but couldn't get it to work

---
+++ Python/Project/model/HeapSort.py
@@ -0,0 +1,15 @@
+class HeapSort:
+
+    def __init__(self, size):
+        from model.MinHeap import MinHeap
+        self.min_heap = MinHeap(size)
+
+    def sort(self, array=[]):
+        output = []
+        for i in range(0, len(array)):
+            self.min_heap.add(array[i])
+        print(self.min_heap.heap)
+        while not self.min_heap.is_empty():
+            output.append(self.min_heap.pop())
+
+        return output
--- Python/Project/model/Main.py
+++ Python/Project/model/Main.py
@@ -4,13 +4,26 @@ def main():
     import time
     from model.DataManager import DataManager
     from model.TimSort import TimSort
+    from model.HeapSort import HeapSort
 
     # instantiate objects to read and sort
     dm = DataManager()
     tim_sorter = TimSort()
 
+    print("--------------------HeapSort Algorithm-------------------------------------------------------------------")
     # read the file
     array_1 = dm.read_file1()
+    heap_sorter = HeapSort(len(array_1))
+    print("\n")
+    print(array_1)
+    start_time = time.time()
+    heap_sorter.sort(array_1)
+    print(array_1)
+    end_time = time.time()
+    total_time = end_time - start_time
+    print("Total time elapsed while sorting array of 10000 integers: ")
+    print(total_time)
+    print("--------------------HeapSort Algorithm-------------------------------------------------------------------")
     array_2 = dm.read_file2()
     array_3 = dm.read_file3()
 
