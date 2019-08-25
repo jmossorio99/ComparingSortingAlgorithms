50480dd284c205aaf418f9456562125efc1e5922
juandavid2025 <juan.aguirre1@correo.icesi.edu.co>
2019-08-25 17:28:11 -0500

    implemented Treesort, class AVLTree, AVLNode ... all working

--- C#/comparingAlgorithmsCSharp/comparingAlgorithmsCSharp/Program.cs
+++ C#/comparingAlgorithmsCSharp/comparingAlgorithmsCSharp/Program.cs
@@ -9,12 +9,12 @@ namespace comparingAlgorithmsCSharp
     class Program
     {
 
-        private readonly String filePath1 = @"set100.txt";
-        private readonly String filePath2 = @"set1000.txt";
-        private readonly String filePath3 = @"set10000 ant.txt";
+        private readonly String FILE_PATH1 = @"set10000.txt";
+        private readonly String FILE_PATH2 = @"set100000.txt";
+        private readonly String fFILE_PATH3 = @"set1000000.txt";
         private static readonly DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
         private Heapsort heapsort;
-        private Timsort timsort;
+        private Treesort treesort;
         private int countAux = 0;
 
         public Program()
@@ -22,31 +22,36 @@ namespace comparingAlgorithmsCSharp
 
             List<int[]> lists = readFiles();
             
-            //Timsort-------------------------------------------------------------------------------------------
+            //Treesort-------------------------------------------------------------------------------------------
 
-            timsort = new Timsort();          
+            treesort = new Treesort();
 
             long startTime = CurrentTimeMillis();            
-            List<int> resultPath1Timsort = timsort.Sort(lists[0]);           
+            List<int> resultPath1Timsort = treesort.Sort(lists[0]);           
             long totalTimePath1Timsort = CurrentTimeMillis() - startTime;
 
+            treesort = new Treesort();
+
             startTime = CurrentTimeMillis();           
-            List<int> resultPath2Timsort = timsort.Sort(lists[1]);        
+            List<int> resultPath2Timsort = treesort.Sort(lists[1]);        
             long totalTimePath2Timsort = CurrentTimeMillis() - startTime;
 
+            treesort = new Treesort();
+
             startTime = CurrentTimeMillis();
-            List<int> resultPath3Timsort = timsort.Sort(lists[2]);
+            List<int> resultPath3Timsort = treesort.Sort(lists[2]);
             long totalTimePath3Timsort = CurrentTimeMillis() - startTime;
 
-            Console.WriteLine("time with 100 elements Timsort = " + totalTimePath1Timsort);
-            Console.WriteLine("time with 1000 elements Timsort = " + totalTimePath2Timsort);
-            Console.WriteLine("time with 10000 elements Timsort = " + totalTimePath3Timsort);
+            
+            Console.WriteLine("time with 10000 elements Treesort = " + totalTimePath1Timsort/1000.0 + "s");
+            Console.WriteLine("time with 100000 elements Treesort = " + totalTimePath2Timsort/1000.0 + "s");
+            Console.WriteLine("time with 1000000 elements Treesort = " + totalTimePath3Timsort/1000.0 + "s");
 
             // Heapsort-------------------------------------------------------------------------------------------
 
             //100
 
-            heapsort = new Heapsort(100);
+            heapsort = new Heapsort(10000);
 
             startTime = CurrentTimeMillis();
             List<int> resultPath1Heapsort = heapsort.Sort(lists[0]);
@@ -54,7 +59,7 @@ namespace comparingAlgorithmsCSharp
 
             //1000
 
-            heapsort = new Heapsort(1000);
+            heapsort = new Heapsort(100000);
 
             startTime = CurrentTimeMillis();
             List<int> resultPath2Heapsort = heapsort.Sort(lists[1]);
@@ -62,16 +67,16 @@ namespace comparingAlgorithmsCSharp
 
             //10000
 
-            heapsort = new Heapsort(10000);
+            heapsort = new Heapsort(1000000);
 
             startTime = CurrentTimeMillis();
             List<int> resultPath3Heapsort = heapsort.Sort(lists[2]);
             long totalTimePath3Heapsort = CurrentTimeMillis() - startTime;
 
             Console.WriteLine("");
-            Console.WriteLine("time with 100 elements Heapsort = " + totalTimePath1Heapsort);
-            Console.WriteLine("time with 1000 elements Heapsort = " + totalTimePath2Heapsort);
-            Console.WriteLine("time with 10000 elements Heapsort = " + totalTimePath3Heapsort);
+            Console.WriteLine("time with 10000 elements Heapsort = " + totalTimePath1Heapsort/1000.0+"s");
+            Console.WriteLine("time with 100000 elements Heapsort = " + totalTimePath2Heapsort/1000.0 + "s");
+            Console.WriteLine("time with 1000000 elements Heapsort = " + totalTimePath3Heapsort/1000.0 + "s");
 
            // ListIntToString(resultPath3Heapsort);
         }
@@ -92,9 +97,9 @@ namespace comparingAlgorithmsCSharp
         {
             List<int[]> answer= new List<int[]>();
 
-            List<String> file1 = File.ReadAllLines(filePath1).ToList();
-            List<String> file2 = File.ReadAllLines(filePath2).ToList();
-            List<String> file3 = File.ReadAllLines(filePath3).ToList();
+            List<String> file1 = File.ReadAllLines(FILE_PATH1).ToList();
+            List<String> file2 = File.ReadAllLines(FILE_PATH2).ToList();
+            List<String> file3 = File.ReadAllLines(fFILE_PATH3).ToList();
 
            
             answer.Add(ListToArray(file1));
